21b6ba67a7272bcd6abde74bbf85e46c05d5b223
AkashAich1999 <108985323+AkashAich1999@users.noreply.github.com>
2024-06-30 18:18:59 +0530

    Rules For Naming Variables

---
+++ Chapter-2-Variables-and-Datatypes/rules_variables.py
@@ -0,0 +1,21 @@
+"""
+RULES FOR CHOOSING AN IDENTIFIER: 
+• A variable name can contain alphabets, digits, and underscores. 
+• A variable name can only start with an alphabet and underscores. 
+• A variable name can’t start with a digit. 
+• No while space is allowed to be used inside a variable name. """
+
+a = 57
+akash99 = 57
+akash_999 = 57
+
+_akash = 57
+print(_akash)
+
+# @akash = 57   (Rule 2)
+# 9akash = 57   (Rule 3)
+
+# a@kash = 57   (Rule 2)
+
+a_kash = 57
+print(a_kash)
