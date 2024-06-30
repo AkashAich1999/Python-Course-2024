1962e26cb2b195ba652334d7ffe1c2d1aff07402
AkashAich1999 <108985323+AkashAich1999@users.noreply.github.com>
2024-06-30 20:02:55 +0530

    Operators

---
+++ Chapter-2-Variables-and-Datatypes/operators.py
@@ -0,0 +1,66 @@
+# Arithmetic Operators
+a = 7
+b = 4
+c = a + b
+print(c)
+
+# Assignment Operators
+a = 4 - 2
+print(a)
+
+b = 7
+b += 3
+print(b)
+
+b -= 3
+print(b)
+
+b *= 3
+print(b) 
+
+b /= 3
+print(b)
+
+# Comparison Operators
+d = 5 < 4
+print(d)
+
+d = 5 > 4
+print(d)
+
+d = 5 < 5
+print(d)
+
+d = 5 <= 5
+print(d)
+
+d = 5 >= 5
+print(d)
+
+d = 5 != 5
+print(d)
+
+d = 5 != 7
+print(d)
+
+d = 5 == 5
+print(d)
+
+# Logical Operators
+e = True or False
+print(e)
+
+# Truth table of 'or' 
+print("True or False: ", True or False)
+print("True or True: ", True or True)
+print("False or True: ", False or True)
+print("False or False: ", False or False)
+
+# Truth table of 'and' 
+print("True and False: ", True and False)
+print("True and True: ", True and True)
+print("False and True: ", False and True)
+print("False and False: ", False and False)
+
+print(not(True))
+print(not(False))
