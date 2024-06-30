4f47a74d64cb4a0cdfefc0e3a8b2c65b2958ec5d
AkashAich1999 <108985323+AkashAich1999@users.noreply.github.com>
2024-06-30 20:42:46 +0530

    type() Function & Typecasting using str(), int(), float()

---
+++ Chapter-2-Variables-and-Datatypes/type.py
@@ -0,0 +1,22 @@
+# type() Function and Typecasting
+
+a = 57
+t = type(a)
+print(t)
+
+a = 7.73
+t = type(a)
+print(t)
+
+a = "Akash Aich"
+t = type(a)
+print(t)
+
+a = "57"
+b = float(a)
+print(type(a))
+print(type(b))
+
+print(type(str(57)))
+print(type(int("57")))
+print(type(float(57)))
