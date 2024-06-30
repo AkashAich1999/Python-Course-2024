740c4ef22f19eeb53bfab0e6488354a01ec4827b
AkashAich1999 <108985323+AkashAich1999@users.noreply.github.com>
2024-06-30 23:16:02 +0530

    Problem4: Greater Than

---
+++ Chapter-2-Practice-Set/Problem4.py
@@ -0,0 +1,6 @@
+# Use comparison operator to find out whether ‘a’ given variable a is greater than ‘b’ or not.
+
+a = int(input("Enter Number1: "))
+b = int(input("Enter Number2: "))
+
+print("a is Greater Than b:", a>b)
