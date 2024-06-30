66127de509c2c996a154582117bc4032771baf19
AkashAich1999 <108985323+AkashAich1999@users.noreply.github.com>
2024-06-30 23:39:14 +0530

    Problem6: Finding Square of a Number

---
+++ Chapter-2-Practice-Set/Problem6.py
@@ -0,0 +1,5 @@
+# python program to calculate the square of a number entered by the user.
+
+a = int(input("Enter Number: "))
+print("Square of the Number: ", a * a)
+print("Square of the Number: ", a ** 2)
