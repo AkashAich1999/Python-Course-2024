79ec69a08e4062e9c2f86d95c0bac7ee52b153dc
AkashAich1999 <108985323+AkashAich1999@users.noreply.github.com>
2024-06-30 17:36:50 +0530

    Datatypes

---
+++ Chapter-2-Variables-and-Datatypes/datatypes.py
@@ -0,0 +1,9 @@
+a = 57  # a is an integer
+
+b = 7.73    # b is a floating-point number
+
+c = "Akash Aich"    # c is a string
+
+d = False   # d is a boolean variable
+
+e = None    # e is a none type variable
