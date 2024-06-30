ac7c8b13660dde39eafc2cfb0f7feb3b2c25c88b
AkashAich1999 <108985323+AkashAich1999@users.noreply.github.com>
2024-06-30 21:09:54 +0530

    input() Function

---
+++ Chapter-2-Variables-and-Datatypes/input.py
@@ -0,0 +1,15 @@
+# Concatenation using input() Function
+a = input("Enter Number 1:")
+b = input("Enter Number 2:")
+
+print("Number 1:", a)
+print("Number 2:", b)
+print("Sum =", a + b)
+
+# Sum using input() Function
+a = int(input("Enter Number 1:"))
+b = int(input("Enter Number 2:"))
+
+print("Number 1:", a)
+print("Number 2:", b)
+print("Sum =", a + b)
