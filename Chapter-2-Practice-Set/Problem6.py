85b24bc3fca102086e641718fb6062ed985f89a3
AkashAich1999 <108985323+AkashAich1999@users.noreply.github.com>
2024-07-01 13:26:11 +0530

    Problem6: Finding Square

--- Chapter-2-Practice-Set/Problem6.py
+++ Chapter-2-Practice-Set/Problem6.py
@@ -3,3 +3,4 @@
 a = int(input("Enter Number: "))
 print("Square of the Number: ", a * a)
 print("Square of the Number: ", a ** 2)
+# print("Square of the Number: ", a ^ 2)  # Incorrect for Finding Square in Python 
