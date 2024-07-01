f4cdbf0227b6ddb64d182ecf238423ac312fbaa9
AkashAich1999 <108985323+AkashAich1999@users.noreply.github.com>
2024-07-01 20:21:25 +0530

    Problem1

---
+++ Chapter-3-Practice-Set/Problem1.py
@@ -0,0 +1,10 @@
+# Python Program to Display a User Entered Name followed by Good Afternoon using input () function.
+
+name = input("Enter Your Name: ")
+
+# New Way
+print(f"Good Afternoon {name} ")
+print(f"Good Afternoon, {name} ")
+
+# Old Way
+print("Good Afternoon, " + name + "")
