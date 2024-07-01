dba4dfb972331a23f2a435a7ef566f48bafeed44
AkashAich1999 <108985323+AkashAich1999@users.noreply.github.com>
2024-07-01 21:58:20 +0530

    Problem4: Replace Double Spaces with Single Space

---
+++ Chapter-3-Practice-Set/Problem4.py
@@ -0,0 +1,6 @@
+# Replace the double space from problem 3 with single spaces.
+
+name = "Akash Aich is a  good  boy"
+print(name.replace("  ", " "))  # Changes are done in a new Copied String.
+
+print(name)  # Strings are Immutable which means that we Cannot Change them by Running Functions on Them. 
