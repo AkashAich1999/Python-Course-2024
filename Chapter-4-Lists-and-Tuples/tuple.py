ea4816cc2ecf45a2cd741c0c5d1205b1c69ceaef
AkashAich1999 <108985323+AkashAich1999@users.noreply.github.com>
2024-07-02 11:44:09 +0530

    Tuples in Python

---
+++ Chapter-4-Lists-and-Tuples/tuple.py
@@ -0,0 +1,21 @@
+# Tuple is an Immutable Data Type in Python.
+
+a = (1, 2, 3, 7)
+print(a)
+print(type(a))
+
+a = ()
+print(a)
+print(type(a))
+
+a = (1)
+print(a)
+print(type(a))
+
+a = (1,)
+print(a)
+print(type(a))
+
+friends = ("Apple", "Orange", 57, 7.73, False, "Akash", "Aich")
+print(friends)
+print(type(friends))
