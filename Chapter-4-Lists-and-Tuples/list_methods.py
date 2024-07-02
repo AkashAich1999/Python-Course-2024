8221203fe7e670d3c45a25e9fd60deae1af4788e
AkashAich1999 <108985323+AkashAich1999@users.noreply.github.com>
2024-07-02 11:06:31 +0530

    List Methods in Python

---
+++ Chapter-4-Lists-and-Tuples/list_methods.py
@@ -0,0 +1,30 @@
+friends = ["Apple", "Orange", 57, 7.73, False, "Akash", "Aich"]
+print(friends)
+
+friends.append("Helpful")
+print(friends)
+
+l1 = [34, 1, 62, 7, 2, 11]
+print(l1)
+
+l1.sort()
+print(l1)
+
+l1.reverse()
+print(l1)
+
+l1.insert(3, 33333)     # insert(index, value)
+print(l1)
+
+l1.pop(0)
+print(l1)
+
+print(l1.pop(0))
+print(l1)
+
+val = l1.pop(1)
+print(val)
+print(l1)
+
+print(l1.remove(7))
+print(l1)
