bfc9c4d45e9ed1ed912364eb8ea09168d0a16732
AkashAich1999 <108985323+AkashAich1999@users.noreply.github.com>
2024-07-02 11:05:55 +0530

    Lists in Python

---
+++ Chapter-4-Lists-and-Tuples/list.py
@@ -0,0 +1,8 @@
+# Python lists are containers to store a set of values of any data type.
+
+friends = ["Apple", "Orange", 57, 7.73, False, "Akash", "Aich"]
+print(friends[0])
+
+friends[0] = "Grapes"   # Unlike Strings, Lists are Mutable.
+print(friends[0])
+print(friends[1:4])
