4b64f043a4ad41ca41f3944e4c3fd96badfb58f9
AkashAich1999 <108985323+AkashAich1999@users.noreply.github.com>
2024-07-02 20:01:44 +0530

    Dictionary in Python

---
+++ Chapter-5-Dictionary-and-Sets/dict.py
@@ -0,0 +1,23 @@
+# Dictionary is a collection of keys-value pairs.
+
+d = {}  # Empty Dictionary
+print(d)
+
+marks = {
+    "Akash": 90,
+    "Darshit": 95,
+    "Manab": 93
+}
+
+print(marks, type(marks))
+print(marks["Akash"])
+
+a = {"key": "value",
+     "akash": "code",
+     "marks": "90",
+     "list": [57, 7.73, 24]    
+}
+
+print(a["key"])
+print(a["akash"])
+print(a["list"])
