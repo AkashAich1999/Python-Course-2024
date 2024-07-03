ef857aa8f20258497bd3af998b87b2db4026b59d
AkashAich1999 <108985323+AkashAich1999@users.noreply.github.com>
2024-07-03 17:21:38 +0530

    Problem2

---
+++ Chapter-5-Practice-Set/Problem2.py
@@ -0,0 +1,29 @@
+# 2. Write a program to input Eight Numbers from the User and Display All the Unique Numbers (Once).
+
+s = set()
+
+n = input("Enter Number 1: ")
+s.add(int(n))
+
+n = input("Enter Number 2: ")
+s.add(int(n))
+
+n = input("Enter Number 3: ")
+s.add(int(n))
+
+n = input("Enter Number 4: ")
+s.add(int(n))
+
+n = input("Enter Number 5: ")
+s.add(int(n))
+
+n = input("Enter Number 6: ")
+s.add(int(n))
+
+n = input("Enter Number 7: ")
+s.add(int(n))
+
+n = input("Enter Number 8: ")
+s.add(int(n))
+
+print(s)
