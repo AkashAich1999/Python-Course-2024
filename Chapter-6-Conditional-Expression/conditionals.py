fd5b41042f441c2e2a0f0ebe6d55d8c5a05a381e
AkashAich1999 <108985323+AkashAich1999@users.noreply.github.com>
2024-07-04 09:42:08 +0530

    if else statement

---
+++ Chapter-6-Conditional-Expression/conditionals.py
@@ -0,0 +1,11 @@
+a = int(input("Enter your age: "))
+
+if(a >= 18):
+    print("You are above the age of consent")
+    print("Good to go ahead!")
+
+else:    
+    print("You are above the age of consent")
+    print("You are not allowed!")
+
+print("\nEnd of Program!")    
