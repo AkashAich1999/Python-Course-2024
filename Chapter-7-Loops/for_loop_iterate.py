7efefb5563a03afa21b0788506d9d08fb9d52840
AkashAich1999 <108985323+AkashAich1999@users.noreply.github.com>
2024-07-04 18:49:51 +0530

    for loop iterate

---
+++ Chapter-7-Loops/for_loop_iterate.py
@@ -0,0 +1,17 @@
+# for loop with Lists
+l = [1, 3, 5, 7, 9, 11]
+
+for i in l:
+    print(i)
+
+# for loop with Tuples
+t = (6, 7, 5, 4)
+
+for i in t:
+    print(i)
+
+# for loop with Strings
+s = "Akash Aich"
+
+for i in s:
+    print(i)
