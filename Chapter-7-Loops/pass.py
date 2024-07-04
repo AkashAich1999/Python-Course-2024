bff25324361ff386b82e603eeb47c3d214abc7b1
AkashAich1999 <108985323+AkashAich1999@users.noreply.github.com>
2024-07-04 19:53:07 +0530

    pass Statement

---
+++ Chapter-7-Loops/pass.py
@@ -0,0 +1,7 @@
+# pass is a null statement in python. 
+# It instructs to “do nothing”. 
+
+l = [7,8, 9] 
+
+for item in l:
+    pass        # without pass, the program will throw an error (IndentationError)
---
+++ Project-1-Snake-Water-Gun_Game/main.py
@@ -0,0 +1,11 @@
+# Requires 2 Players both have to select something from Snake or Water or Gun.
+
+# Following are the rules of the game:
+# Snake vs. Water: Snake drinks the water hence wins.
+# Water vs. Gun: The gun will drown in water, hence a point for water
+# Gun vs. Snake: Gun will kill the snake and win.
+# If Both Players choose same thing, then it will be a Draw.
+
+
+
+
