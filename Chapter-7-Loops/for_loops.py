2cfbe4d9f1770ad2436bb4fe9bff5520486e0d8b
AkashAich1999 <108985323+AkashAich1999@users.noreply.github.com>
2024-07-04 18:28:57 +0530

    for loops

---
+++ Chapter-7-Loops/for_loops.py
@@ -0,0 +1,14 @@
+for i in range(0, 7):
+    print(i)
+
+for i in range(7):
+    print(i)
+
+l = [7, 8, 9]
+for item in l:
+    print(item)
+
+# range(start, stop, step_size) 
+# step_size is usually not used with range()
+for i in range(0, 100, 5):
+    print(i)       
