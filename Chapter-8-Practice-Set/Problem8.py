6c25ab80ae37745de43e7d7a6d02fd1c278d749d
AkashAich1999 <108985323+AkashAich1999@users.noreply.github.com>
2024-07-09 15:25:48 +0530

    Problem8: Print Multiplication Table of a number using function

---
+++ Chapter-8-Practice-Set/Problem8.py
@@ -0,0 +1,17 @@
+# 8. Write a python function to print multiplication table of a given number.
+
+# def multiply(n):
+#     for i in range(1,n+1):
+#         print(f"{n} X {i} = {n * i}")
+
+# multiply(9)  
+
+def multiply(n):
+    for i in range(1, 11):
+        print(f"{n} X {i} = {n * i}")   
+    print("\n")        
+
+multiply(3)        
+multiply(5)        
+multiply(7)        
+multiply(9)        
