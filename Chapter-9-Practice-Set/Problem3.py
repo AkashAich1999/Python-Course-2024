e23cdaeeaf41dca503d14d34356df99e7b9f60f5
AkashAich1999 <108985323+AkashAich1999@users.noreply.github.com>
2024-07-12 16:10:13 +0530

    Problem3

--- Chapter-9-Practice-Set/Problem3.py
+++ Chapter-9-Practice-Set/Problem3.py
@@ -6,7 +6,7 @@ def generateTable(n):
     for i in range(1, 11):
         table += f"{n} X {i} = {n * i}\n"
 
-    with open(f"tables/table_{n}", "w") as f:
+    with open(f"tables/table_{n}.txt", "w") as f:
         f.write(table)
 
 for i in range(2, 21):
