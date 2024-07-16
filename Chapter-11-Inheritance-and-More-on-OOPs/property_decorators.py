81e9a8f63d0f0fdc32107bdf5374be496fd8552a
AkashAich1999 <108985323+AkashAich1999@users.noreply.github.com>
2024-07-16 16:58:36 +0530

    @name.setter

--- Chapter-11-Inheritance-and-More-on-OOPs/property_decorators.py
+++ Chapter-11-Inheritance-and-More-on-OOPs/property_decorators.py
@@ -1,3 +1,28 @@
+# class Employee:
+#     a = 1
+
+#     @classmethod
+#     def show(cls):
+#         print(f"The class attribute of a is: {cls.a}")
+
+#     @property
+#     def name(self):
+#         return self.ename
+    
+#     @name.setter
+#     def name(self, value):
+#         self.ename = value
+
+# e = Employee()
+# e.a = 45
+
+# print(Employee.name)    # <property object at 0x0000018B7090DCB0>
+# e.name = "Akash Aich"   
+# print(e.name)           # Akash Aich
+# print(Employee.name)    # <property object at 0x0000018B7090DCB0>
+
+# e.show()
+
 class Employee:
     a = 1
 
@@ -7,11 +32,12 @@ class Employee:
 
     @property
     def name(self):
-        return self.ename
+        return f"{self.fname} {self.lname}"
     
     @name.setter
     def name(self, value):
-        self.ename = value
+        self.fname = value.split(" ")[0]
+        self.lname = value.split(" ")[1]
 
 e = Employee()
 e.a = 45
