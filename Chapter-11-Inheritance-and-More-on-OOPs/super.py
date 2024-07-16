d267778f24b3d74e4a517bdc723d33cf87a9286f
AkashAich1999 <108985323+AkashAich1999@users.noreply.github.com>
2024-07-16 13:12:19 +0530

    super() Method

---
+++ Chapter-11-Inheritance-and-More-on-OOPs/super.py
@@ -0,0 +1,48 @@
+# class Employee:
+#     def __init__(self):
+#         print("Constructor of Employee !")
+#     a = 1
+
+# class Programmer(Employee):
+#     def __init__(self):
+#         print("Constructor of Programmer !")
+#     b = 2
+
+# class Manager(Programmer):
+#     def __init__(self):
+#         print("Constructor of Manager !")
+#     c = 3
+
+# e = Employee()
+# print(e.a)
+
+# p = Programmer()
+# print(p.a, p.b)
+
+# m = Manager()
+# print(m.a, m.b, m.c)
+
+class Employee:
+    def __init__(self):
+        print("Constructor of Employee !")
+    a = 1
+
+class Programmer(Employee):
+    def __init__(self):
+        print("Constructor of Programmer !")
+    b = 2
+
+class Manager(Programmer):
+    def __init__(self):
+        super().__init__()
+        print("Constructor of Manager !")
+    c = 3
+
+e = Employee()
+print(e.a)
+
+p = Programmer()
+print(p.a, p.b)
+
+m = Manager()
+print(m.a, m.b, m.c)
