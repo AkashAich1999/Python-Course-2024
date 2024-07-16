1d2aa132c276863bb470687dc1a6b014b29982b9
AkashAich1999 <108985323+AkashAich1999@users.noreply.github.com>
2024-07-16 12:44:54 +0530

    Multilevel Inheritance

---
+++ Chapter-11-Inheritance-and-More-on-OOPs/multilevel_inheritance.py
@@ -0,0 +1,17 @@
+class Employee:
+    a = 1
+
+class Programmer(Employee):
+    b = 2
+
+class Manager(Programmer):
+    c = 3
+
+e = Employee()
+print(e.a)
+
+p = Programmer()
+print(p.a, p.b)
+
+m = Manager()
+print(m.a, m.b, m.c)
