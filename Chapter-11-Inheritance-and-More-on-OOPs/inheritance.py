2c0d2480d578638641fc758912572bb33aed7dc0
AkashAich1999 <108985323+AkashAich1999@users.noreply.github.com>
2024-07-16 09:49:20 +0530

    inheritance introduction

--- Chapter-11-Inheritance-and-More-on-OOPs/inheritance.py
+++ Chapter-11-Inheritance-and-More-on-OOPs/inheritance.py
@@ -18,15 +18,23 @@
 
 class Employee:              # Base Class or Parent Class
     company = "ITC"
+    name = "Default Name"
+    salary = 100000
     def show(self):
         print(f"The name of the Employee is {self.name} and the salary is {self.salary}")
 
 class Programmer(Employee):  # Derived Class or Child Class
     company = "ITC Infotech"
+    name = "Default Name"
+    language = "Python"
     def showLanguage(self):
-        print(f"The name is {self.name} and he is good with {self.language} language.")
+        print(f"The name is {self.name} and He is Good with {self.language} Language.")
 
 a = Employee()
 b = Programmer()
 
 print(a.company, b.company)
+
+a.show()
+b.show()
+b.showLanguage()
