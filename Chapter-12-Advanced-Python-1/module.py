5ec73d7c792facebaf09f75a3f9c521bc2673a1d
AkashAich1999 <108985323+AkashAich1999@users.noreply.github.com>
2024-07-19 15:42:48 +0530

    __name__ == "__main__"

---
+++ Chapter-12-Advanced-Python-1/module.py
@@ -0,0 +1,9 @@
+def myFunc():
+    print("Hello!")
+
+myFunc()
+print(__name__)
+
+if __name__ == "__main__":
+    # If This Code is directly Executed by Running the file its present in.
+    print("main.py File is Executed!")
