b57c5e8c62032b392549e60befc0f932f7a1ba2f
AkashAich1999 <108985323+AkashAich1999@users.noreply.github.com>
2024-07-19 12:06:32 +0530

    parenthesised context manager using with statement

---
+++ Chapter-12-Advanced-Python-1/parenthesised_context_manager.py
@@ -0,0 +1,8 @@
+# We can use multiple context managers in a single with statement more cleanly using the parenthesised context manager.
+
+# with (
+#     open('file1') as f1,
+#     open('file2') as f2
+# ):
+    
+    # Process Files
