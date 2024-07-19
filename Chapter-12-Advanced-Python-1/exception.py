7fded32a836a3506ad3144add40d13cc3de39e75
AkashAich1999 <108985323+AkashAich1999@users.noreply.github.com>
2024-07-19 13:55:14 +0530

    Exception Handling

--- Chapter-12-Advanced-Python-1/exception.py
+++ Chapter-12-Advanced-Python-1/exception.py
@@ -1,8 +1,29 @@
+# try:
+#     a = int(input("Hey, Enter a Number: "))
+#     print(a)
+
+# except Exception as e:      # This will Prevent Application Crashing
+#     print(e)
+
+# print("Thank You!")  
+
 try:
     a = int(input("Hey, Enter a Number: "))
     print(a)
 
-except Exception as e:      # This will Prevent Application Crashing
-    print(e)
+except ValueError as v:
+    print("Hey!")
+    print(v)
+
+except ZeroDivisionError as z:
+    print("Hi!")
+    print(z)
+
+except TypeError as t:
+    print("Hello!")
+    print(t)    
+
+except Exception as e:
+    print(e)    
 
 print("Thank You!")    
