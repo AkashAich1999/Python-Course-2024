79e4e072b5dc43668cf46de35ae639f988fbc76c
AkashAich1999 <108985323+AkashAich1999@users.noreply.github.com>
2024-07-19 10:17:59 +0530

    Variable Type Hint & Function Type Hints

---
+++ Chapter-12-Advanced-Python-1/types_1.py
@@ -0,0 +1,9 @@
+# variable type hint
+age : int = 25
+name : str = "Akash Aich"
+
+# function type hints
+def greeting(name: str) -> str:
+    return f"Hello, {name}!"
+
+print(greeting("Akash Aich"))   # Output: Hello, Akash Aich!
