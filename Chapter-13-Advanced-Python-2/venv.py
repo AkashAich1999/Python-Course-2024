63bd6b5248007dab76a27c74d54df541c3c588b1
AkashAich1999 <108985323+AkashAich1999@users.noreply.github.com>
2024-07-20 16:06:57 +0530

    Virtual Environment

---
+++ Chapter-13-Advanced-Python-2/venv.py
@@ -0,0 +1,21 @@
+# Inside Terminal:
+# PS D:\zzzzz Latest Projects\Python-Course-2024\Chapter-13-Advanced-Python-2> pip install virtualenv
+# PS D:\zzzzz Latest Projects\Python-Course-2024\Chapter-13-Advanced-Python-2> virtualenv env
+# PS D:\zzzzz Latest Projects\Python-Course-2024\Chapter-13-Advanced-Python-2> .\env\Scripts\activate.ps1
+# (env) PS D:\zzzzz Latest Projects\Python-Course-2024\Chapter-13-Advanced-Python-2> pip install pandas
+# 
+# (env) PS D:\zzzzz Latest Projects\Python-Course-2024\Chapter-13-Advanced-Python-2> python
+# >>> import pandas
+# >>> pandas.__version__
+# '2.2.2'
+# 
+# exit
+# exit (This will close the Terminal)
+# 
+# deactivate
+# 
+# pip freeze
+# 
+# pip freeze > requirements.txt
+#
+# pip install -r .\requirements.txt
