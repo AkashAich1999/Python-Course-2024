6c10d02382c205aea2f25ff3621b36d2973c928a
AkashAich1999 <108985323+AkashAich1999@users.noreply.github.com>
2024-07-20 16:24:18 +0530

    format method (strings)

---
+++ Chapter-13-Advanced-Python-2/format.py
@@ -0,0 +1,8 @@
+a = "{} is a good {}".format("Akash", "Engineer")
+print(a)
+
+b = "{0} is a good {1}".format("Akash", "Engineer")
+print(b)
+
+c = "{1} is a good {0}".format("Engineer", "Akash")
+print(c)
