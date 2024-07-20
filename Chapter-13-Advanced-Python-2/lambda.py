e11c0a95defbca8f9983c7f8091dfcdbcf698feb
AkashAich1999 <108985323+AkashAich1999@users.noreply.github.com>
2024-07-20 16:14:37 +0530

    Lambda Functions

---
+++ Chapter-13-Advanced-Python-2/lambda.py
@@ -0,0 +1,13 @@
+# def square(n):
+#     return n*n
+
+# print(square(7))
+
+# lambda arguments:expressions 
+# can be used as a normal function 
+
+square = lambda x:x*x
+print(square(7))
+
+sum = lambda a,b,c:a+b+c
+print(sum(2,3,5))
