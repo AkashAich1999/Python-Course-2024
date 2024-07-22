62aff5099033cd0fc8d7689e9c52ea6d1066210a
AkashAich1999 <108985323+AkashAich1999@users.noreply.github.com>
2024-07-22 13:06:35 +0530

    Jarvis Project

---
+++ Project-3-Jarvis-Virtual-Assistant/musicLibrary.py
@@ -0,0 +1,5 @@
+music = {
+    "night": "https://www.youtube.com/watch?v=syFZfO_wfMQ&ab_channel=OneDirectionVEVO",
+    "one": "https://www.youtube.com/watch?v=GFGPSx6cPN0&ab_channel=BlueVEVO",
+    "bebe": "https://www.youtube.com/watch?v=cpKolP6mMec&ab_channel=harshlikhariofficial2007"
+}
